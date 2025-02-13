fb0838f1a25877069f861a865b9cf0aa340a4dd8
SevenProxy <flapbraga@gmail.com>
2025-02-13 15:45:47 -0300

    Build in netlify.

--- src/components/footer/footer_information.rs
+++ src/components/footer/footer_information.rs
@@ -17,7 +17,7 @@ impl Component for FooterInformation {
           <a href="linkedin.com/in/deivid-braga-086601264/" target="_blank" class="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
             <img src="/public/link.svg" alt="icons" width="20" height="20" />
           </a>
-          <a href="https://github.com/Cardosofiles" target="_blank" class="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+          <a href="https://github.com/SevenProxy" target="_blank" class="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
             <img src="/public/git.svg" alt="icons" width="20" height="20" />
           </a>
           <a href="discord.com/users/1193791291125940309" target="_blank" class="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
